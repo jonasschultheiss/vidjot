41676c37c4b70027c150777ca127cc818fd64d92
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2018-12-18 13:26:24 +0100

    idk

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Index Route
+app.get('/', (req, res) => {
+  res.send('<h1>Moser lutscht Schwanz</h1>');
+});
+
 const port = 5000;
 
 app.listen(port, () => {
