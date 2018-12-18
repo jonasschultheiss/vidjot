02cd0993d3e06650ef7bfdbe9f724c9e2dff8429
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2018-12-18 13:50:36 +0100

    idkdik

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('<h1>Moser lutscht Schwanz</h1>');
+  res.send('<h1 style="margin:0 auto;width:50%;padding: 10px;">Moser lutscht Schwanz</h1>');
 });
 
 const port = process.env.PORT || 8080;
