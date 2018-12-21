112b48b25652fd0b3f06c60f17db4d6f48ac2027
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2018-12-21 17:04:57 +0100

    made some more more progress

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Map global promise - get red of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost:27017/vidjot-dev', {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -9,12 +23,44 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
-  res.render('index', {
-    title
-  });
+  res.render('index', { title: title });
+});
+
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details' });
+  }
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      });
+  }
 });
 
 // About route
@@ -22,7 +68,7 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`server started on ${port}`);
