cd17f2024e77acb0041acf85ae9c66f7533deb5b
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2018-12-21 14:54:32 +0100

    made some progress in the course

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
-// How middleware works
-app.use();
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX ');
+  const title = 'Welcome';
+  res.render('index', {
+    title
+  });
 });
 
 // About route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  res.render('about');
 });
 
 const port = process.env.PORT || 8080;
